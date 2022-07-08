8d5c0e082dc794124142ff52a2349d83f4385198
proloypaul <proloyp23@gmail.com>
2022-07-08 11:55:25 +0600

    set up react tailwind

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Home section</h1>
+            <h1 className="text-3xl font-bold underline">Home section</h1>
         </div>
     );
 };
